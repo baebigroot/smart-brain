fad23fbf6fa4cae0600c5062babca94c264d44cd
baebigroot <kieunhung1229@gmail.com>
2021-05-12 09:56:26 +0200

    updating server URL

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -25,7 +25,7 @@ class Register extends React.Component {
 
     onSubmitSignIn = (event) => {
         event.preventDefault();
-        fetch('http://localhost:3001/register', {
+        fetch('https://arcane-scrubland-85882.herokuapp.com/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -20,7 +20,7 @@ class SignIn extends React.Component{
 
     onSubmitSignIn = (event) => {
         event.preventDefault();
-        fetch('http://localhost:3001/signin', {
+        fetch('https://arcane-scrubland-85882.herokuapp.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
